b48742fe2d15b1d0c91d606a56fc0632236f4504
Andy Yuan <andy.yuan2tdsb@gmail.com>
2026-07-26 23:55:17 -0400

    Update __init__.py to include new imports

--- src/spatial-extremes/__init__.py
+++ src/spatial-extremes/__init__.py
@@ -1,1 +1,10 @@
 # init file
+from .gev import GEVResult, fit_gev, fit_gev_by_site
+from .smith import SmithModel
+from .dependence import fmadogram, empirical_extremal_coefficient, binned_extremal_coefficient
+
+__all__ = [
+    "GEVResult", "fit_gev", "fit_gev_by_site",
+    "SmithModel",
+    "fmadogram", "empirical_extremal_coefficient", "binned_extremal_coefficient",
+]
